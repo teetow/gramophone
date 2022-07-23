dd6cae833d689ee2dcf02cd0bf24e61683b9d2a9
Johan Althoff <teetow@gmail.com>
2022-07-23 20:16:35 +0200

    chore: add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/gramophone/",
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
@@ -11,5 +12,4 @@ export default defineConfig({
       },
     }),
   ],
-  
 });
