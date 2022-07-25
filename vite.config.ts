8d05fbb94b8e42ab3b0fa5d36d7cb14d4f4901d4
Johan Althoff <teetow@gmail.com>
2022-07-25 11:15:23 +0200

    fix: better builds

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: "/gramophone/",
+  base: "./",
+  build: {
+    assetsDir: "./",
+  },
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
